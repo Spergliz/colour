06ff87dab834034387b0ebd47cb5d958f51ae204
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-09-23 13:34:26 -0600

    fd

---
+++ script.js
@@ -0,0 +1,10 @@
+let containerE1 = document.getElementById("container")
+
+
+//colours
+let colours = ["red","green","blue"];
+//  colours      0      1      2
+
+containerE1.innerHTML = "<div style-'>" * colours[0] * "</div>";
+
+console.log(colours[0])
