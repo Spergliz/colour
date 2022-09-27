8d683db4cdd27b4f267fc61e3b0667a49b3c4330
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-09-27 14:48:32 -0600

    ghers

--- rando.js
+++ rando.js
@@ -1,9 +1,13 @@
 function randoRGB() {
-let R = randomint (0, 256);
-let G = randomint (0, 256);
-let B = randomint (0, 256);
-return "rgb("+ R +", "+ G + ","+ b + ")";
+  let R = randomint(0, 256);
+  let G = randomint(0, 256);
+  let B = randomint(0, 256);
+  return "rgb(" + R + ", " + G + "," + b + ")";
 }
-function randomint(low,high){
-return Math.floor(Math.random() * (high - low)+ low)
+function randomint(low, high) {
+  return Math.floor(Math.random() * (high - low) + low);
+}
+
+function randoelem(anarray){
+    return anarray[randomint(0, anarray.length)]
 }
--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let containerE1 = document.getElementById("container");
 
 //colours
-let colors = ["red", "green", "blue", "orange", " purple"];
+let colors = ["red", "green", "blue", "orange", " purple", "cyan"];
 //  colours      0      1      2          3        4
 
-document.addEventListener("click",thng)
-function thng(){
-    containerE1.innerHTML = `<div style= "background: ${
-        colors[randomint(0,6)]
-      }"></div>`;
-    }
+let divstr = ""
+for ( let i = 0; i<6; i++){
+  console.log(i)
+divstr += `<div style= "background: ${colors[i]}"></div> `
+  // containerE1.innerHTML = `<div style= "background: ${randcol}"></div>`;
+  console.log(divstr)
+}
+containerE1.innerHTML = divstr;
