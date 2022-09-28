2c9d3c0931af5e09f8c1326999e8291593382952
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-09-28 14:47:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ let containerE1 = document.getElementById("container");
 let inputE1 = document.getElementById("color-in");
 
 inputE1.addEventListener("keydown", handle);
-//colours
-let colors = ["red", "green", "blue", "orange", " purple", "cyan"];
-//  colours      0      1      2          3        4
 
 function handle(event) {
   if (event.keyCode === 13) {
     let inputstr = inputE1.value;
     let colorsarray = inputstr.split(",");
-    display(colorsarray)
+    display(colorsarray);
   }
 }
-function display(colors){
-let divstr = "";
-for (let i = 0; i < 6; i++) {
-  console.log(i);
-  divstr += `<div style= "background: ${colors[i]}"></div> `;
-  // containerE1.innerHTML = `<div style= "background: ${randcol}"></div>`;
-  console.log(divstr);
-}
-containerE1.innerHTML = divstr;
+
+y;
+function display(colors) {
+  let divstr = "";
+  for (let i = 0; i < 6; i++) {
+    console.log(i);
+    divstr += `<div style= "background: ${colors[i]}"></div> `;
+    // containerE1.innerHTML = `<div style= "background: ${randcol}"></div>`;
+    console.log(divstr);
+  }
+  containerE1.innerHTML = divstr;
 }
